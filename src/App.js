977168e5655616da833c3f9d98982809025218b0
zerobeloved <799818616@qq.com>
2022-02-25 10:35:58 +0800

    add toggle reminder to server

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ const App = () => {
         return data
     }
 
+    //Fetch task
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await res.json()
+
+        return data
+    }
+
     //Add Task
     const addTask = async(task) => {
         // const id = Math.floor(Math.random() * 10000) + 1
@@ -49,10 +57,23 @@ const App = () => {
     }
 
     //Toggle reminder
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`,  {
+            method:'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
+        const data = await res.json()
+
         setTasks(
             tasks.map((task) =>
-            task.id === id ? {...task, reminder: !task.reminder} :task
+            task.id === id ? {...task, reminder: data.reminder} :task
                 )
             )
         }
